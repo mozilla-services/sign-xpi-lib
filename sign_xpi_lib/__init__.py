81e7f3b4ff047f2e77c7078efa8eb9dab21ae4c0
Ethan Glasser-Camp <ethan@betacantrips.com>
2017-07-07 14:28:19 -0400

    __init__: flake8

--- sign_xpi_lib/__init__.py
+++ sign_xpi_lib/__init__.py
@@ -2,10 +2,9 @@
 
 """Top-level package for sign-xpi-lib."""
 
+from sign_xpi_lib.sign_xpi_lib import XPIFile
+
 __author__ = """Ethan Glasser-Camp"""
 __email__ = 'eglassercamp@mozilla.com'
 __version__ = '0.1.0'
-
-from sign_xpi_lib.sign_xpi_lib import XPIFile
-
 __all__ = ['XPIFile']
